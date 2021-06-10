729d582d8bc881707c07d8d118e8768d3d537b7d
Mike Liu <mike.liu@icw.io>
2021-06-10 19:05:49 +0800

    update: form test

--- src/tests/LoginForm.test.js
+++ src/tests/LoginForm.test.js
@@ -10,6 +10,48 @@ describe('Tests <LoginForm/>', () => {
   })
 
   it('Types input values', () => {
-    cy.get('input[type="email"]').should('have.value', '')
+    cy.log(`** should be empty input **`)
+
+    cy.get('input[name="email"]').should('have.value', '')
+    cy.get('input[name="password"]').should('have.value', '')
+    cy.get('input[name="tel"]').should('have.value', '')
+
+
+    cy.log(`** types values **`)
+
+    const vals = ['test@email.com', '123ps', '12345678']
+
+    cy.get('input[name="email"]').type(vals[0]).should('have.value', vals[0])
+    cy.get('input[name="password"]').type(vals[1]).should('have.value', vals[1])
+    cy.get('input[name="tel"]').type(vals[2]).should('have.value', vals[2])
+  })
+
+
+  it('Types input values & submit form', () => {
+    cy.log(`** should be empty input **`)
+
+    cy.get('input[name="email"]').should('have.value', '')
+    cy.get('input[name="password"]').should('have.value', '')
+    cy.get('input[name="tel"]').should('have.value', '')
+
+
+    cy.log(`** types values **`)
+
+    const vals = ['test@email.com', '123ps', '12345678']
+
+    cy.get('input[name="email"]').type(vals[0]).should('have.value', vals[0])
+    cy.get('input[name="password"]').type(vals[1]).should('have.value', vals[1])
+    cy.get('input[name="tel"]').type(vals[2]).should('have.value', vals[2])
+
+
+    cy.log(`** click submit **`)
+
+    cy.get('button[type="submit"]').click()
+
+
+    cy.log(`** submit button should change to loading **`)
+
+    cy.get('button[type="submit"]').should('have.value', 'Submitting...')
+
   })
 })
